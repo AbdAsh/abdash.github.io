8431aa843052d819093d03622ef94c09b97e88b9
Abd Ash <59181667+AbdAsh@users.noreply.github.com>
2022-10-26 13:46:54 +0300

    Coming Soon.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <Tutorial />
+    <div>
+        <h1>Coming Soon</h1>
+        <Loading />
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+    name: 'IndexPage',
 }
 </script>
+
+<style lang="scss">
+body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    max-height: 100vh;
+    color: #fff;
+    background-color: #000;
+    font-family: 'Cairo', sans-serif;
+    font-size: 1.5rem;
+    font-weight: 300;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
