d6f4ec96778ee1940e8b725b1d6cc673402b65f0
Abd Ash <a.mahmutoglu@cyberneticlabs.io>
2026-02-18 01:39:57 +0300

    Remove unneeded files

--- test/Loading.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Loading from '@/components/Loading.vue'
-
-describe('Loading', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Loading)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
